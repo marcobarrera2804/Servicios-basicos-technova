e16b92f77b0b9ef3e677eb08d7f4a9f5592361fc
marcobarrera2804 <marcobarrera2804@gmail.com>
2025-03-29 05:11:45 -0600

    Update punto3.php

--- srv/punto3.php
+++ srv/punto3.php
@@ -1,14 +1,12 @@
 <?php
-
-require_once __DIR__ . "/../lib/php/devuelveJson.php";
-
-// Establecer el encabezado para indicar que se está devolviendo JSON
 header('Content-Type: application/json');
 
-// Devolver el JSON con la información del artista
-devuelveJson([
-    "nombre" => "The Weeknd",
-    "genero" => "R&B, Pop",
-    "album_mas_vendido" => "After Hours"
-]);
+// Datos que se enviarán como respuesta
+$respuesta = [
+    'origen' => 'Tech Nova',
+    'mensaje' => 'Hola pp.',
+    'boton' => 'Saludos❤️'
+];
+
+echo json_encode($respuesta);
 ?>
